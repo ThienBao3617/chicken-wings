05f31b2bcdfecfe352e2753bae6e748bef0c94f3
ThienBao3617 <nltb12042003@gmail.com>
2026-06-06 05:33:03 +0700

    Create landing page structure

---
+++ src/features/landing/HeroSection.tsx
@@ -0,0 +1,12 @@
+export function HeroSection() {
+  return (
+    <section>
+      <h1>Chicken Wings</h1>
+
+      <p>
+        Nơi những nhà làm phim trẻ Việt Nam gặp nhau,
+        cùng tạo ra các dự án điện ảnh và truyền thông.
+      </p>
+    </section>
+  );
+}
